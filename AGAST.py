4038ec4bd5b9f9d1c940b370f9912720ff6f240a
tkC0ding <tkjagani@gmail.com>
2024-12-26 19:04:37 +0530

    another one

--- AGAST.py
+++ AGAST.py
@@ -4,6 +4,9 @@ img = cv2.imread("lena.jpg")
 
 agast = cv2.AgastFeatureDetector_create()
 
+agast.setNonmaxSuppression(True)
+agast.setThreshold(50)
+
 kp = agast.detect(img, None)
 
 cv2.drawKeypoints(img, kp, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
